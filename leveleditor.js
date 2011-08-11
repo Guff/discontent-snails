3bf9ea89561598741dba57bd6578efc6ac3b9cd3
Cass <kasull88@gmail.com>
2011-08-11 15:25:05 -0400

    Updated to match status of game

--- leveleditor.js
+++ leveleditor.js
@@ -1,39 +1,41 @@
 var canvas;
 var ctx;
 var textures;
-var level;
 
 function initEditor() {
     canvas = document.getElementById("leveleditor");
     ctx = canvas.getContext("2d");
     
-    textures = { block: new Image() };
+    textures = { block: new Image(), slingshot: new Image(), bg: new Image(),
+                 ground: new Image(), snail: new Image() };
     textures.block.src = "data/wooden-block-40x10.png";
+    textures.slingshot.src = "data/slingshot.png";
+    textures.bg.src = "data/sky-bg.png";
+    textures.ground.src = "data/ground.png";
+    textures.snail.src = "data/snail-normal.png";
     drawLevel(ctx);
-}
-
-function canvasToCp(pos) {
-    return { x: pos.x - canvas.width / 2, y: pos.y - canvas.height / 2 };
-}
-
-function cpToCanvas(pos) {
-    return { x: pos.x + canvas.width / 2, y: pos.y + canvas.height / 2 };
 }
 
 function drawLevel(ctx) {
     var level = JSON.parse(document.levelJSON.levelJSONtext.value);
-    ctx.fillStyle = "#000000";
-    ctx.rect(0, 0, canvas.width, canvas.height);
-    ctx.fill();
+    ctx.drawImage(textures.bg, 0, 0);
+    ctx.drawImage(textures.ground, 0, 430);
+    drawSlingshot(ctx, level.slingshot);
     level.obstacles.forEach(function (body) { drawBody(ctx, body) });
 }
 
+function drawSlingshot(ctx, slingshot) {
+    ctx.save();
+    ctx.translate(slingshot.x, slingshot.y);
+    ctx.scale(1 / 3, 1 / 3);
+    ctx.drawImage(textures.slingshot, -7, -120);
+    ctx.restore();
+}
+
 function drawBody(ctx, body) {
-    ctx.fillStyle = "#ffffff";
-    var pos = cpToCanvas({ x: body.x, y: body.y });
     var angle = body.angle / 180 * Math.PI;
     ctx.save();
-    ctx.translate(pos.x, pos.y);
+    ctx.translate(body.x, body.y);
     ctx.rotate(angle);
     ctx.scale(0.4, 0.4);
     ctx.drawImage(textures.block, -50, -12.5);

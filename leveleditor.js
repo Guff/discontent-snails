d874d40f01753fb764709bde2ee419ac8dd30b7f
Cass <kasull88@gmail.com>
2011-08-11 17:03:17 -0400

    Now the level editor can actually move things around

--- leveleditor.js
+++ leveleditor.js
@@ -1,10 +1,13 @@
 var canvas;
 var ctx;
 var textures;
+var level;
 
 function initEditor() {
     canvas = document.getElementById("leveleditor");
     ctx = canvas.getContext("2d");
+    
+    level = JSON.parse(document.levelJSON.levelJSONtext.value);
     
     textures = { block: new Image(), slingshot: new Image(), bg: new Image(),
                  ground: new Image(), snail: new Image() };
@@ -13,15 +16,67 @@ function initEditor() {
     textures.bg.src = "data/sky-bg.png";
     textures.ground.src = "data/ground.png";
     textures.snail.src = "data/snail-normal.png";
-    drawLevel(ctx);
+    drawLevel(ctx, level);
 }
 
-function drawLevel(ctx) {
-    var level = JSON.parse(document.levelJSON.levelJSONtext.value);
+function getBoundingBoxes(level) {
+    boundingBoxes = new Array();
+    
+    boundingBoxes[0] = { body: level.slingshot, x: level.slingshot.x - 7,
+                         y: level.slingshot.y - 40, w: 13, h: 40 };
+    for (i = 0; i < level.obstacles.length; i++) {
+        obstacle = level.obstacles[i];
+        boundingBoxes.push({ body: obstacle, x: obstacle.x - 20,
+                             y: obstacle.y - 5, w: 40, h: 10 });
+    }
+    return boundingBoxes;
+}
+
+function mouseOverBody(body, e) {
+    var x = e.pageX - canvas.offsetLeft;
+    var y = e.pageY - canvas.offsetTop;
+    var bx0 = body.x, bx1 = body.x + body.w;
+    var by0 = body.y, by1 = body.y + body.h;
+    if (x >= bx0 && x <= bx1 && y >= by0 && y <= by1) {
+        return body;
+    } else {
+        return null;
+    }
+}
+
+function onMouseMove(body, e) {
+    body.body.x = e.pageX - canvas.offsetLeft;
+    body.body.y = e.pageY - canvas.offsetTop;
+    drawLevel(ctx, level);
+    document.levelJSON.levelJSONtext.value = JSON.stringify(level);
+}
+
+function onMouseDown(level, e) {
+    var boundingBoxes = getBoundingBoxes(level);
+    for (i = 0; i < boundingBoxes.length; i++) {
+        var body = mouseOverBody(boundingBoxes[i], e);
+        if (body) {
+            break;
+        }
+    }
+    if (!body) {
+        return;
+    }
+    
+    canvas.onmousemove = function (e) { onMouseMove(body, e); };
+}
+
+function onMouseUp(e) {
+    canvas.onmousemove = null;
+}
+
+function drawLevel(ctx, level) {
     ctx.drawImage(textures.bg, 0, 0);
     ctx.drawImage(textures.ground, 0, 430);
     drawSlingshot(ctx, level.slingshot);
-    level.obstacles.forEach(function (body) { drawBody(ctx, body) });
+    level.obstacles.forEach(function (body) { drawBody(ctx, body); });
+    canvas.onmousedown = function (e) { onMouseDown(level, e); };
+    canvas.onmouseup = onMouseUp;
 }
 
 function drawSlingshot(ctx, slingshot) {

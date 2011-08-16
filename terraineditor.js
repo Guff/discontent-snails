dbfae557690caea27cf6efeb3e123c3b18a8f9ce
Cass <kasull88@gmail.com>
2011-08-16 13:37:11 -0400

    made the editor gobble up scroll and keydown events

--- terraineditor.js
+++ terraineditor.js
@@ -17,3 +17,7 @@ function drawTerrain(ctx, terrain, pattern) {
     ctx.strokeStyle = stoneBorder;
     ctx.stroke();
 }
+
+function distance(a, b) {
+    return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
+}
